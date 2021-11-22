c0bcfa783b9742b6c5652320c0ef183e93a06f89
Zyrael <etoyaleha@gmail.com>
2021-11-23 00:07:12 +0300

    first step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,18 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import genDiff from '../src/index.js';
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0')
   .option('-f, --format', '[type]  output format')
-  .argument('<file1>')
-  .argument('<file2>');
+  .arguments('<file1> <file2>')
+  .action(() => {
+    const [file1, file2] = program.args;
+
+    const diff = genDiff(file1, file2);
+    console.log(diff);
+  });
 
 program.parse();
---
+++ src/index.js
@@ -0,0 +1,38 @@
+import path from 'path';
+import { openSync, readFileSync } from 'fs';
+import _ from 'lodash';
+
+const getFileData = (file) => {
+  const filepath = path.resolve(file);
+  const openedFile = openSync(filepath);
+  const data = readFileSync(openedFile, 'utf-8');
+  return JSON.parse(data);
+};
+
+const getSortedKeys = (data) => {
+  const keys = Object.keys(data);
+  return _.sortBy(keys);
+};
+
+export default (file1, file2) => {
+  const data1 = getFileData(file1);
+  const data2 = getFileData(file2);
+
+  const sorted1 = getSortedKeys(data1);
+  const sorted2 = getSortedKeys(data2);
+
+  const unique = Array.from(new Set([...sorted1, ...sorted2]));
+
+  const result = unique.reduce((acc, key) => {
+    if (key in data1 && key in data2) {
+      return (data1[key] === data2[key]
+        ? `${acc}    ${key}: ${data1[key]}\n`
+        : `${acc}  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}\n`);
+    }
+    return (key in data1
+      ? `${acc}  - ${key}: ${data1[key]}\n`
+      : `${acc}  + ${key}: ${data2[key]}\n`);
+  }, '');
+
+  return `{\n${result}}`;
+};
