07b0ab06272465084f33885a28324df865c7afc3
Zyrael <etoyaleha@gmail.com>
2021-11-27 17:00:00 +0300

    make getDifference function

--- src/diff.js
+++ src/diff.js
@@ -6,25 +6,43 @@ const getSortedKeys = (data) => {
   return _.sortBy(keys);
 };
 
-export default (file1, file2) => {
-  const data1 = parseFile(file1);
-  const data2 = parseFile(file2);
-
+const getDifference = (data1, data2, repeats = 1) => {
   const sorted1 = getSortedKeys(data1);
   const sorted2 = getSortedKeys(data2);
 
   const unique = Array.from(new Set([...sorted1, ...sorted2]));
 
-  const result = unique.reduce((acc, key) => {
+  const spaces = ' '.repeat(repeats);
+  return unique.reduce((acc, key) => {
+    if (_.isObject(data1[key]) && _.isObject(data2[key])) {
+      return getDifference(data1[key], data2[key], repeats + 2);
+    }
     if (key in data1 && key in data2) {
-      return (data1[key] === data2[key]
-        ? `${acc}    ${key}: ${data1[key]}\n`
-        : `${acc}  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}\n`);
+      return (data1[key] === data2[key])
+        ? [...acc, [`${spaces} `, [key, data1[key]]]]
+        : [...acc, [`${spaces}-`, [key, data1[key]]], [`${spaces}+`, [key, data2[key]]]];
     }
     return (key in data1
-      ? `${acc}  - ${key}: ${data1[key]}\n`
-      : `${acc}  + ${key}: ${data2[key]}\n`);
-  }, '');
-
-  return `{\n${result}}`;
+      ? [...acc, [`${spaces}-`, [key, data1[key]]]]
+      : [...acc, [`${spaces}+`, [key, data2[key]]]]);
+  }, []);
 };
+export default (file1, file2) => {
+  const data1 = parseFile(file1);
+  const data2 = parseFile(file2);
+
+  return getDifference(data1, data2);
+};
+
+// const result = unique.reduce((acc, key) => {
+//   if (key in data1 && key in data2) {
+//     return (data1[key] === data2[key]
+//       ? `${acc}    ${key}: ${data1[key]}\n`
+//       : `${acc}  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}\n`);
+//   }
+//   return (key in data1
+//     ? `${acc}  - ${key}: ${data1[key]}\n`
+//     : `${acc}  + ${key}: ${data2[key]}\n`);
+// }, '');
+//
+// return `{\n${result}}`;
